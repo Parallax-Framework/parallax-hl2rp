a1f137213ecbb2d507ebee751bff1be5f34cd86b
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-07-16 14:14:06 +0300

    Update boot.lua

--- modules/custom_module/boot.lua
+++ modules/custom_module/boot.lua
@@ -4,6 +4,6 @@ MODULE.Name = "Custom Module"
 MODULE.Ddescription = "Adds custom functionality to the HL2RP schema."
 MODULE.Author = "YourName"
 
-function MODULE:Initialize()
+function MODULE:PostInitializeModule()
     ax.util:Print("Custom Module has been loaded.")
 end
