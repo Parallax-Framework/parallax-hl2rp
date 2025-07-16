b53a43b0bbf8c5461746cad1d0ee43696b038ef9
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-07-16 14:06:56 +0300

    Refactor PlayerUse function to improve readability and maintain logic flow

--- schema/hooks/sv_hooks.lua
+++ schema/hooks/sv_hooks.lua
@@ -24,8 +24,7 @@ end
 
 function SCHEMA:PlayerUse(client, entity)
     local isAdminFaction = client:Team() == FACTION_ADMIN
-    if ( client:IsCombine() or isAdminFaction ) then
-        if ( !entity:HasSpawnFlags(256) and !entity:HasSpawnFlags(1024) and !entity:OnCooldown("Use") and entity:GetClass() == "func_door" ) then
+    if ( client:IsCombine() or isAdminFaction and entity:GetClass() == "func_door" and !entity:OnCooldown("Use") and ( !entity:HasSpawnFlags(256) and !entity:HasSpawnFlags(1024) ) ) then
             entity:Fire("Open", "", 0)
             entity:SetCooldown("Use", 1)
             entity:EmitSound("buttons/combine_button1.wav", 70, 100, 0.5, CHAN_AUTO)
