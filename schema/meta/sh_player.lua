325e3252010ceb77b8331a453c41eba62dd8c3bd
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-07-17 20:33:23 +0300

    Update sh_player.lua

--- schema/meta/sh_player.lua
+++ schema/meta/sh_player.lua
@@ -3,3 +3,11 @@ local PLAYER = FindMetaTable("Player")
 function PLAYER:IsCombine()
     return self:Team() == FACTION_MPF or self:Team() == FACTION_OTA
 end
+
+function PLAYER:IsMetrocop()
+    return self:Team() == FACTION_MPF
+end
+
+function PLAYER:IsOWSoldier()
+    return self:Team() == FACTION_OTA
+end
