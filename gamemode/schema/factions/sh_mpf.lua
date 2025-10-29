a129609ad2b2ade44eede5f5040825aa295d04f2
riggs9162 <riggs9162@gmx.de>
2025-10-29 05:12:18 +0100

    feat(factions-mpf,ota): revamp setup, sounds, taglines; migrate keys

--- gamemode/schema/factions/sh_mpf.lua
+++ gamemode/schema/factions/sh_mpf.lua
@@ -2,15 +2,31 @@ FACTION.name = "Metropolice Force"
 FACTION.description = "Implanted human collaborators who police the urban zones for the Combine. They impose curfews, break up dissent and suppress resistance with lethal efficiency."
 FACTION.color = Color(45, 90, 140)
 
-FACTION.image = ax.util:GetMaterial("gamepadui/hl2/chapter3")
+FACTION.image = ax.util:GetMaterial("parallax/hl2rp/banners/mpf.png")
 
 FACTION.models = {
     "models/police.mdl"
 }
 
-FACTION.AllowNonAscii = true
+FACTION.deathSound = {
+    "npc/metropolice/die1.wav",
+    "npc/metropolice/die2.wav",
+    "npc/metropolice/die3.wav"
+}
 
-FACTION.Taglines = {
+FACTION.painSound = {
+    "npc/metropolice/pain1.wav",
+    "npc/metropolice/pain2.wav",
+    "npc/metropolice/pain3.wav",
+    "npc/metropolice/pain4.wav",
+    "npc/metropolice/pain5.wav",
+    "npc/metropolice/pain6.wav"
+}
+
+FACTION.allowNonAscii = true
+FACTION.allowSkinCustomization = false
+
+FACTION.taglines = {
     "DEFENDER",
     "HERO",
     "JURY",
@@ -28,14 +44,9 @@ FACTION.Taglines = {
 }
 
 function FACTION:GetDefaultName(client)
-    return "MPF:" .. ax.config:Get("city.abbreviation") .. "-" .. self.Taglines[math.random(#self.Taglines)] .. "-" .. ax.util:ZeroNumber(math.random(1, 9999), 4), true
+    return "MPF:" .. ax.config:Get("cityAbbreviation") .. "-" .. self.taglines[math.random(#self.taglines)] .. "-" .. ax.util:PadNumber(math.random(1, 9999), 4), true
 end
 
 FACTION_MPF = FACTION.index
 
 ax.animations:SetModelClass("models/police.mdl", "metrocop")
-ax.animations:SetModelClass("models/police/eliteghostcp.mdl", "metrocop")
-ax.animations:SetModelClass("models/police/eliteshockcp.mdl", "metrocop")
-ax.animations:SetModelClass("models/police/leet_police2.mdl", "metrocop")
-ax.animations:SetModelClass("models/police/policetrench.mdl", "metrocop")
-ax.animations:SetModelClass("models/police/sect_police2.mdl", "metrocop")
--- gamemode/schema/factions/sh_ota.lua
+++ gamemode/schema/factions/sh_ota.lua
@@ -2,7 +2,7 @@ FACTION.name = "Overwatch Transhuman Arm"
 FACTION.description = "Genetically-augmented shock troops deployed when civilian forces fail. Their purpose is the decisive annihilation of hostile elements and fortification of Combine interests."
 FACTION.color = Color(150, 15, 15)
 
-FACTION.image = ax.util:GetMaterial("gamepadui/hl2/chapter4")
+FACTION.image = ax.util:GetMaterial("parallax/hl2rp/banners/ota.png")
 
 FACTION.models = {
     "models/combine_soldier.mdl",
@@ -10,4 +10,51 @@ FACTION.models = {
     "models/combine_soldier_prisonguard.mdl"
 }
 
+FACTION.deathSound = {
+    "npc/combine_soldier/die1.wav",
+    "npc/combine_soldier/die2.wav",
+    "npc/combine_soldier/die3.wav",
+    "npc/combine_soldier/die4.wav"
+}
+
+FACTION.painSound = {
+    "npc/combine_soldier/pain1.wav",
+    "npc/combine_soldier/pain2.wav",
+    "npc/combine_soldier/pain3.wav",
+    "npc/combine_soldier/pain4.wav",
+    "npc/combine_soldier/pain5.wav",
+    "npc/combine_soldier/pain6.wav"
+}
+
+FACTION.allowNonAscii = true
+FACTION.allowSkinCustomization = false
+
+FACTION.taglines = {
+    "BLADE",
+    "FIST",
+    "FLASH",
+    "HAMMER",
+    "HUNTER",
+    "LEADER",
+    "RANGER",
+    "RAZOR",
+    "SAVAGE",
+    "SCAR",
+    "SLASH",
+    "SPEAR",
+    "STAB",
+    "SWEEPER",
+    "SWIFT",
+    "SWORD",
+    "TRACKER"
+}
+
+function FACTION:GetDefaultName(client)
+    return "OTA:" .. ax.config:Get("cityAbbreviation") .. "-" .. self.taglines[math.random(#self.taglines)] .. "-" .. ax.util:PadNumber(math.random(1, 9999), 4), true
+end
+
 FACTION_OTA = FACTION.index
+
+ax.animations:SetModelClass("models/combine_soldier.mdl", "overwatch")
+ax.animations:SetModelClass("models/combine_super_soldier.mdl", "overwatch")
+ax.animations:SetModelClass("models/combine_soldier_prisonguard.mdl", "overwatch")
