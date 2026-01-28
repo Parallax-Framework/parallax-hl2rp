59afdd0db6308edf278276548e9d794d903e8cb4
Riggs <riggs9162@gmx.de>
2026-01-28 23:07:34 +0100

    fix(hooks): adjust post-processing color settings for better visual effects

--- gamemode/schema/hooks/cl_hooks.lua
+++ gamemode/schema/hooks/cl_hooks.lua
@@ -2,9 +2,9 @@ local color = {}
 color["$pp_colour_addr"] = 0
 color["$pp_colour_addg"] = 0
 color["$pp_colour_addb"] = 0
-color["$pp_colour_brightness"] = -0.01
-color["$pp_colour_contrast"] = 1.35
-color["$pp_colour_colour"] = 0.65
+color["$pp_colour_brightness"] = -0.05
+color["$pp_colour_contrast"] = 1.25
+color["$pp_colour_colour"] = 0.75
 color["$pp_colour_mulr"] = 0
 color["$pp_colour_mulg"] = 0
 color["$pp_colour_mulb"] = 0
@@ -14,14 +14,14 @@ function SCHEMA:RenderScreenspaceEffects()
     local client = ax.client
     local character = client:GetCharacter()
     if ( !character ) then return end
-    
+
     local health, healthMax = client:Health(), client:GetMaxHealth()
     local healthFraction = math.Clamp(health / healthMax, 0, 1)
     local healthFractionInv = 1 - healthFraction
 
     color["$pp_colour_brightness"] = -0.05 - (0.05 * healthFractionInv)
-    color["$pp_colour_contrast"] = 1.5 - (0.5 * healthFractionInv)
-    color["$pp_colour_colour"] = 0.65 * healthFraction
+    color["$pp_colour_contrast"] = 1.25 - (0.5 * healthFractionInv)
+    color["$pp_colour_colour"] = 0.75 * healthFraction
 
     DrawColorModify(color)
 
