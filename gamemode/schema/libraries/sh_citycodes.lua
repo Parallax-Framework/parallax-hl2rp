1f8ba0c99463cd4d9e704f49faa294c191883598
bloodycop <bloodycop6385@gmail.com>
2026-04-15 00:12:27 +0300

    feat: city code table

---
+++ gamemode/schema/libraries/sh_citycodes.lua
@@ -0,0 +1,6 @@
+SCHEMA.CityCodes = {
+    {name = "Preserved",            color = Color(100, 255, 100)},
+    {name = "Unrest",               color = Color(255, 250, 100)},
+    {name = "Autonomous Judgment",  color = Color(255, 128, 55)},
+    {name = "Judgment Waiver",      color = Color(255, 35, 35)},
+}
