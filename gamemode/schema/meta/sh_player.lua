f09a9f22c436cdc014725f8e20a1ac4773123f50
Riggs <riggs9162@gmx.de>
2026-05-03 13:31:21 +0200

    refactor(player): simplify player meta function definitions

--- gamemode/schema/meta/sh_player.lua
+++ gamemode/schema/meta/sh_player.lua
@@ -1,11 +1,13 @@
-function ax.player.meta:IsCombine()
+local player = ax.player.meta or FindMetaTable("Player")
+
+function player:IsCombine()
     return self:Team() == FACTION_MPF or self:Team() == FACTION_OTA
 end
 
-function ax.player.meta:IsMetrocop()
+function player:IsMetrocop()
     return self:Team() == FACTION_MPF
 end
 
-function ax.player.meta:IsOWSoldier()
+function player:IsOWSoldier()
     return self:Team() == FACTION_OTA
 end
